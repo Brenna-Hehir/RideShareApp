5d5bf7a7cadbd91912a63624a69710f47e209d7a
Brenna-Hehir <brennahehir@gmail.com>
2025-04-22 00:22:14 -0400

    unused imports removed

--- app/src/main/java/edu/uga/cs/rideshareapp/MainActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapp/MainActivity.java
@@ -1,27 +1,12 @@
 package edu.uga.cs.rideshareapp;
 
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
