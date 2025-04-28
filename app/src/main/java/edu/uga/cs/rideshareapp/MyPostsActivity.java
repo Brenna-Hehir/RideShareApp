38cb81e98e3b8eb41a02c5d838c5a8ab7dafc464
Brenna-Hehir <brennahehir@gmail.com>
2025-04-28 15:01:52 -0400

    UI added

--- app/src/main/java/edu/uga/cs/rideshareapp/MyPostsActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapp/MyPostsActivity.java
@@ -14,7 +14,7 @@ public class MyPostsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_posts);
+        setContentView(R.layout.activity_my_posts);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
