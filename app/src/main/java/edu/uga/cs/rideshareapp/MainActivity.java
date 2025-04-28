c6c4d8f5515c189ffc462de797b4c5f25f9d2334
Brenna-Hehir <brennahehir@gmail.com>
2025-04-28 14:39:35 -0400

    files added and renamed

--- app/src/main/java/edu/uga/cs/rideshareapp/MainActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapp/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         rideFormButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RideForm.class);
+                Intent intent = new Intent(MainActivity.this, RideFormActivity.class);
                 startActivity(intent);
             }
         });
